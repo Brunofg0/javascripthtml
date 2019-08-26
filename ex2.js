3c9e4b8ceb55cd40a42a908fe9edeb08951b1ecc
Bruno Figueiredo <brunofg0@gmail.com>
2019-08-26 20:32:20 -0300

    Ex2 editado

--- ex2.js
+++ ex2.js
@@ -14,12 +14,16 @@ function funcao() {
     var somaSalario = 0
     var somaFilhos = 0
     var maiorSalario = vet[0].salario
+    var qtd = 0
     for (var i=0; i<vet.lenght; i++){
        somaSalario = somaSalario + vet[i].salario
        somaFilhos = somafilhos + vet[i].filhos
        if (vet[i].salario > maiorSalario){
            maiorSalario = vet[i].salario
        }
+       if ((vet[i].sexo == "F") && (vet[i].salario > 1000))
+       qtd++
     }
-    alert("Media: " + somaSalario/vet.lenght + "\nFilhos: " + somaFilhos/vet.lenght + "\nMaior salario: " + maiorSalario)
+    alert("Media: " + somaSalario/vet.lenght + "\nFilhos: " + somaFilhos/vet.lenght + "\nMaior salario: " + maiorSalario + 
+    "\nMulheres com salario maior que R$ 1000: " + qtd/3*100)
 }
