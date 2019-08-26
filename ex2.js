38024c130f3224dfc05bf2cafcfc06e2caa2e8dc
Bruno Figueiredo <brunofg0@gmail.com>
2019-08-26 20:42:24 -0300

    Att ex2

--- ex2.js
+++ ex2.js
@@ -11,19 +11,22 @@ function funcao() {
         vet.push(objeto)
 
     }
-    var somaSalario = 0
-    var somaFilhos = 0
-    var maiorSalario = vet[0].salario
-    var qtd = 0
+    var somaSalario = 0;
+    var somaFilhos = 0;
+    var maiorSalario = vet[0].salario;
+    var qtd = 0;
     for (var i=0; i<vet.lenght; i++){
        somaSalario = somaSalario + vet[i].salario
-       somaFilhos = somafilhos + vet[i].filhos
+       somaFilhos = somaFilhos + vet[i].filhos
        if (vet[i].salario > maiorSalario){
            maiorSalario = vet[i].salario
        }
-       if ((vet[i].sexo == "F") && (vet[i].salario > 1000))
-       qtd++
+       if ((vet[i].sexo == "F") && (vet[i].salario > 1000)){
+            qtd++;
+       }
     }
-    alert("Media: " + somaSalario/vet.lenght + "\nFilhos: " + somaFilhos/vet.lenght + "\nMaior salario: " + maiorSalario + 
-    "\nMulheres com salario maior que R$ 1000: " + qtd/3*100)
 }
+alert("Media: " + somaSalario/vet.lenght +
+ "\nFilhos: " + somaFilhos/vet.lenght + 
+ "\nMaior salario: " + maiorSalario + 
+ "\nPorcentagem de mulheres com salario maior que R$ 1000: " + qtd/3*100)
