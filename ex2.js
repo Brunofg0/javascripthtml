0ef88b06d7effdd1215f8e8f7a4c86ef0d69fd17
Bruno Figueiredo <brunofg0@gmail.com>
2019-08-26 20:27:10 -0300

    Add ex2!

---
+++ ex2.js
@@ -0,0 +1,25 @@
+function funcao() {
+    var vet = {}
+    for (var i=0; i<3; i++){
+        var objeto = {}
+        // Coloca dados no objeto
+        objeto.salario = Number(prompt("Salario: "))
+        objeto.idade = Number(prompt("Idade: "))
+        objeto.filhos = Number(prompt("Filhos: "))
+        objeto.sexo = Number(prompt("Sexo: "))
+        // Coloca objetos no vetor
+        vet.push(objeto)
+
+    }
+    var somaSalario = 0
+    var somaFilhos = 0
+    var maiorSalario = vet[0].salario
+    for (var i=0; i<vet.lenght; i++){
+       somaSalario = somaSalario + vet[i].salario
+       somaFilhos = somafilhos + vet[i].filhos
+       if (vet[i].salario > maiorSalario){
+           maiorSalario = vet[i].salario
+       }
+    }
+    alert("Media: " + somaSalario/vet.lenght + "\nFilhos: " + somaFilhos/vet.lenght + "\nMaior salario: " + maiorSalario)
+}
